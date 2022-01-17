8727ff523b39e99df8fcb406b86ae49e0e241aec
Wagner Lisboa <wgr.lisboa@gmail.com>
2022-01-17 17:15:31 -0300

    #InProgress - add class for expand button login

--- scipts.js
+++ scipts.js
@@ -1,8 +1,9 @@
 const send = () => {
   const getElementContainer = document.getElementById("container");
-  const getElementBtnSend = document.getElementById("btn-send");
   getElementContainer.classList.toggle("active");
-  // getElementBtnSend.innerHTML = "";
-  console.log(`getElementBtnSend`, getElementBtnSend);
-  console.log("+++++", getElementContainer);
+
+  setTimeout(() => {
+    getElementContainer.classList.remove("active");
+    getElementContainer.classList.toggle("expand");
+  }, 2000);
 };
