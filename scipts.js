fb034a8a97025332be4bcc53a4f7d123de1ca30a
Wagner Lisboa <wgr.lisboa@gmail.com>
2022-01-18 00:48:25 -0300

    #Close - Finalizando

--- scipts.js
+++ scipts.js
@@ -1,9 +1,12 @@
 const send = () => {
   const getElementContainer = document.getElementById("container");
+  const getElementButtom = document.getElementById("btn-send");
+  getElementButtom.firstElementChild.innerHTML = "Enviar";
   getElementContainer.classList.toggle("active");
 
   setTimeout(() => {
     getElementContainer.classList.remove("active");
     getElementContainer.classList.toggle("expand");
+    getElementButtom.firstElementChild.innerHTML = "Fechar";
   }, 2000);
 };
